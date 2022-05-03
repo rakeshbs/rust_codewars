4c899da3cbe5aa2217756c404cca917e4826405e
rakeshbs <rakeshbs@gmail.com>
2022-05-03 17:07:23 +0530

    Refactored validbraces

--- validbraces.rs
+++ validbraces.rs
@@ -1,32 +1,22 @@
 fn valid_braces(s: &str) -> bool {
-    let opening_braces: Vec<char> = vec!['(', '{', '['];
-    let closing_braces: Vec<char> = vec![')', '}', ']'];
     let mut stack: Vec<char> = Vec::new();
     for c in s.chars() {
-        if opening_braces.contains(&c) {
-            stack.push(c)
-        } else {
-            if let Some(top) = stack.pop() {
-                let ind1 = closing_braces.iter().position(|&curr| curr == c).unwrap();
-                let ind2 = opening_braces.iter().position(|&curr| curr == top).unwrap();
-
-                if ind1 != ind2 {
+        match c {
+            '{' => stack.push('}'),
+            '(' => stack.push(')'),
+            '[' => stack.push(']'),
+            x => {
+                if Some(x) != stack.pop() {
                     return false;
                 }
-            } else {
-                return false;
             }
         }
     }
-    if stack.is_empty() {
-        return true;
-    };
-    false
+    return stack.is_empty();
 }
-
 fn main() {
     expect_true("()");
-    expect_false("(((({{");
+    expect_false("}}}");
 }
 
 fn expect_true(s: &str) {
